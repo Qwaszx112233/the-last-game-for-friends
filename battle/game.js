e8ee74ac959b54e24cc6fb1de6f3e2f2ab643abb
Qwaszx112233 <ryabinin.sergei.alekseevich@gmail.com>
2025-12-05 02:50:12 +0200

    Add files via upload

--- battle/game.js
+++ battle/game.js
@@ -1,15 +1,1 @@
-
-class Game {
-    endBattle() {
-        const result = {
-            xp: this.player.totalXP,
-            resources: {
-                steel: this.collectedSteel,
-                energy: this.collectedEnergy,
-                tech: this.collectedTech || 0
-            }
-        };
-        localStorage.setItem("lastBattleResult", JSON.stringify(result));
-        window.location.href = "../base/base.html";
-    }
-}
+// Stage1 game.js
